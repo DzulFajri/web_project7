917cbc334a592a6985cb4dbc8c813a92b042d425
root <root@LAPTOP-8VQEK5PU>
2024-05-06 14:54:52 +0700

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [Frontend::class, 'home'])->name('frontend');
